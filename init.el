18a7e559985dc4f5b085a82c8744e102ee0e55d7
Nao Taco <naotaco@gmail.com>
2015-10-24 13:39:42 +0900

    Add env for golang

--- init.el
+++ init.el
@@ -11,3 +11,26 @@
 
 (el-get-bundle auto-complete)
 (el-get-bundle perl6-mode)
+(el-get-bundle go-mode)
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+; must have godef command. run 'go get github.com/rogpeppe/godef' to get it.
+(add-hook 'go-mode-hook (lambda ()
+			  (local-set-key (kbd "M-.") 'godef-jump)))
+
+(add-hook 'go-mode-hook
+	  (lambda ()
+	    (setq-default)
+	    (setq tab-width 2)
+	    (setq standard-indent 2)
+	    (setq indent-tabs-mode nil)))
+
+;must have go-autocomplete. run 'go get -u github.com/nsf/gocode'
+(add-to-list 'load-path (substitute-in-file-name "${GOPATH}/src/github.com/nsf/gocode/emacs"))
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+
+; go get -u github.com/dougm/goflymake 
+(add-to-list 'load-path (substitute-in-file-name "${GOPATH}/src/github.com/dougm/goflymake"))
+(require 'go-flymake)
+
