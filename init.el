5fa1c311d767f60ec041247654a049eaf06764b7
Nao Taco <naotaco@gmail.com>
2016-02-28 01:22:58 +0900

    update

--- init.el
+++ init.el
@@ -1,67 +1,68 @@
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+;; load el-get
+(add-to-list 'load-path "~/.emacs.d/el-get")
+(require 'el-get)
 
-(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-;; basic settings.
-(define-key mode-specific-map "c" 'compile)
-(menu-bar-mode -1)
-(setq completion-ignore-case t)
-(custom-set-variables
- '(read-file-name-completion-ignore-case t))
-
-(setq-default show-trailing-whitespace t)
-
-;; highlight current line
-(defface hlline-face
-  '((((class color)
-      (background dark))
-     (:background "black" :foreground "white"))
-    (((class color)
-      (background light))
-     (:background "Blue"))
-    (t
-     ()))
-           "*Face used by hl-line.")
-(setq hl-line-face 'hlline-face)
-(global-hl-line-mode)
-
-;; install bundles
+(el-get-bundle cl-lib)
 (el-get-bundle auto-complete)
+(el-get-bundle anything)
 (el-get-bundle perl6-mode)
 (el-get-bundle go-mode)
 (add-hook 'before-save-hook 'gofmt-before-save)
 
-;; must have godef command. run 'go get github.com/rogpeppe/godef' to get it.
-(add-hook 'go-mode-hook
-	  (lambda ()
-	    (local-set-key (kbd "M-.") 'godef-jump)
-	    (setq-default)
-	    (setq tab-width 2)
-	    (setq standard-indent 2)
-	    (setq indent-tabs-mode nil)))
+;; basic settings
+(define-key mode-specific-map "c" 'compile)
+(menu-bar-mode -1)
+(setq completion-ignore-case t)
 
-;;must have go-autocomplete. run 'go get -u github.com/nsf/gocode'
-(add-to-list 'load-path (substitute-in-file-name "${GOPATH}/src/github.com/nsf/gocode/emacs"))
-(require 'go-autocomplete)
-(require 'auto-complete-config)
+(setq-default show-trailing-whitespace t)
 
-;; go get -u github.com/dougm/goflymake
-(add-to-list 'load-path (substitute-in-file-name "${GOPATH}/src/github.com/dougm/goflymake"))
-(require 'go-flymake)
+;; highlighted line number settings
+(el-get-bundle elpa:hlinum)
+(custom-set-faces
+ '(linum ((t (:foreground "white"
+			  :background "black")))))
+(setq linum-format "%4d  ")
+(hlinum-activate)
+(custom-set-faces
+ '(linum-highlight-face ((t (:foreground "pink"
+					 :background "black")))))
+;; activate settings
+(custom-set-variables
+ '(read-file-name-completion-ignore-case t)
+ '(global-linum-mode t))
 
-(add-hook 'perl6-mode-hook
-	  (lambda ()
-	    (set (make-local-variable 'compile-command)
-		 (concat "perl6 " buffer-file-name))))
+;;;; Language specified settings
+
+((lambda ()
+   ;; must have godef command. run 'go get github.com/rogpeppe/godef' to get it.
+   (add-hook 'go-mode-hook
+	     (lambda ()
+	       (setq-default)
+	       (setq tab-width 2)
+	       (setq standard-indent 2)
+	       (setq indent-tabs-mode nil)
+	       (if (not (string-match "go" compile-command))
+		   (set (make-local-variable 'compile-command)
+			(concat "go build -v " (buffer-file-name))))
+	       (local-set-key (kbd "M-.") 'godef-jump)
+	       ))
+
+   ;; must have go-autocomplete. run 'go get -u github.com/nsf/gocode'
+   ;; (el-get-bundle elpa:go-autocomplete)
+   ;; (require 'go-autocomplete)
+   ;; (require 'auto-complete-config)
+
+   ;; get -u github.com/dougm/goflymake
+   ;; (add-to-list 'load-path (substitute-in-file-name "/home/nao/go_work/src/github.com/dougm/goflymake")) ;
+   ;; (require 'go-flymake)
+
+   ))
+
+;; perl5
+(el-get-bundle perl-completion)
 
 ;; C/C++
+(require 'cc-mode)
 (add-hook 'c-mode-common-hook 'flycheck-mode)
 (add-hook 'c-mode-common-hook
 	  (lambda ()
@@ -73,9 +74,45 @@
 	    )
 	  )
 
-;; git
+;; perl-mode
+(add-hook 'perl-mode-hook
+	  '(lambda ()
+	     (make-local-variable 'compile-command)
+	     (setq compile-command
+		   (concat "perl " (buffer-file-name)))
+	     ))
+(add-hook 'perl6-mode-hook
+	  '(lambda ()
+	     (make-local-variable 'compile-command)
+	     (setq compile-command
+		   (concat "perl6 " (buffer-file-name)))
+	     ))
+
+;; yaml
+(el-get-bundle 'yaml-mode)
+
 (el-get-bundle magit)
 (require 'magit)
 
-;; markdown
-(el-get-bundle markdown-mode)
+;; md
+(el-get-bundle jrblevin/markdown-mode)
+
+(el-get-bundle dockerfile-mode)
+
+;; http://d.hatena.ne.jp/a_bicky/20140104/1388822688
+(el-get-bundle helm
+  (helm-mode 1)
+  (require 'helm-config)
+  (helm-mode 1)
+  (define-key global-map (kbd "M-x")     'helm-M-x)
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  (define-key global-map (kbd "C-x C-r") 'helm-recentf)
+  (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+  (define-key global-map (kbd "C-c i")   'helm-imenu)
+  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+  (define-key global-map (kbd "M-r")     'helm-resume)
+  (define-key global-map (kbd "C-M-h")   'helm-apropos)
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+  )
